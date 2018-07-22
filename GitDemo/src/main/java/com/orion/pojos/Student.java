a48ba963ef067d76b0f398c9c6bf440590f8a84b
Shree <Shree@Shree-PC>
2018-07-22 22:24:44 +0530

    Commited by Soundarya

---
+++ GitDemo/src/main/java/com/orion/pojos/Student.java
@@ -0,0 +1,12 @@
+package com.orion.pojos;
+
+public class Student {
+	private int studId;
+	private String studName;
+	public Student(int studId, String studName) {
+		super();
+		this.studId = studId;
+		this.studName = studName;
+	}
+	
+}
