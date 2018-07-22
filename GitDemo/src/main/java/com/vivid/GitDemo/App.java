d2a6cb26821aefd47a066eef14e0888c965ba61d
Ninad <mynameisninad@gmail.com>
2018-07-22 21:28:24 +0530

    commit to local 1st

---
+++ GitDemo/src/main/java/com/vivid/GitDemo/App.java
@@ -0,0 +1,13 @@
+package com.vivid.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Orions..!" );
+    }
+}
