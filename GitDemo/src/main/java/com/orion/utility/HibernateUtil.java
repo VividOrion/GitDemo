e0a185bf8edf1894e5e53cf3a747c967c6d64c99
Tanmayeee <tanmayee.chin@gmail.com>
2018-07-22 22:32:16 +0530

    Hibernate util package

---
+++ GitDemo/src/main/java/com/orion/utility/HibernateUtil.java
@@ -0,0 +1,38 @@
+package com.orion.utility;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	private static SessionFactory sf=null;
+	public static SessionFactory getFactory()
+	{
+			if(sf==null)
+			{
+				sf=new Configuration().configure().buildSessionFactory();
+			}
+			return sf;
+		}
+		public static void flushNCommit(Session se,Transaction tr)
+		{
+			if(se!=null)
+			{
+				se.flush();
+			}
+			if(tr!=null)
+			{
+				tr.commit();
+			}
+		}
+		public static void sessionClose(Session se)
+		{
+			if(se!=null)
+			{
+				se.close();
+			}
+			
+		}
+	}
+
